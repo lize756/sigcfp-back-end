3b0df414fa9de9178b18e3a85ffc71e9c38a8cf7
Oscar Ivan Riascos G <osvan202@gmail.com>
2022-03-06 13:31:55 -0500

    Merge to rest-implementation

---
+++ logic/src/main/java/com/edu/icesi/sigcfp/sigcfpbackendbusiness/logic/services/implementations/AcademicstudyService_BASE_4779.java
@@ -0,0 +1,45 @@
+package com.edu.icesi.sigcfp.sigcfpbackendbusiness.logic.services.implementations;
+
+import com.edu.icesi.sigcfp.sigcfpbackendbusiness.entity.entities.Academicstudy;
+import com.edu.icesi.sigcfp.sigcfpbackendbusiness.logic.services.interfaces.IAcademicstudyService;
+import com.edu.icesi.sigcfp.sigcfpbackendbusiness.persistence.repositories.interfaces.IAcademicstudyRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AcademicstudyService implements IAcademicstudyService {
+
+    IAcademicstudyRepo iAcademicstudyRepo;
+
+    @Autowired
+    public AcademicstudyService(IAcademicstudyRepo iAcademicstudyRepo) {
+        this.iAcademicstudyRepo = iAcademicstudyRepo;
+    }
+
+    @Override
+    public Academicstudy addAcademicstudy(Academicstudy academicstudy) {
+        return null;
+    }
+
+    @Override
+    public Academicstudy updateAcademicstudy(Academicstudy academicstudy) {
+        return null;
+    }
+
+    @Override
+    public Academicstudy searchAcademicstudy(long acadStudId) {
+        return null;
+    }
+
+    @Override
+    public Academicstudy deleteAcademicstudy(long acadStudId) {
+        return null;
+    }
+
+    @Override
+    public List<Academicstudy> academicstudies() {
+        return null;
+    }
+}
