8387158a97ae195199d8d89b7b4f2c03c3866c7b
Oss21 <47828398+Oss21@users.noreply.github.com>
2022-05-10 11:26:00 -0500

    Correct problem relation with getListInternRequests

--- rest/src/main/java/com/edu/icesi/sigcfp/sigcfpbackendbusiness/rest/implementations/InternRequestController.java
+++ rest/src/main/java/com/edu/icesi/sigcfp/sigcfpbackendbusiness/rest/implementations/InternRequestController.java
@@ -28,6 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.edu.icesi.sigcfp.sigcfpbackendbusiness.config.HelpClassMethod;
 import com.edu.icesi.sigcfp.sigcfpbackendbusiness.entity.entities.Career;
+import com.edu.icesi.sigcfp.sigcfpbackendbusiness.entity.entities.Company;
 import com.edu.icesi.sigcfp.sigcfpbackendbusiness.entity.entities.InternRequest;
 import com.edu.icesi.sigcfp.sigcfpbackendbusiness.logic.services.interfaces.IInternRequestService;
 import com.edu.icesi.sigcfp.sigcfpbackendbusiness.rest.interfaces.IInternRequestController;
@@ -42,12 +43,12 @@ import org.springframework.web.bind.annotation.*;
 //@PreAuthorize("")
 public class InternRequestController implements IInternRequestController {
 
-    private IInternRequestService iInternRequestService;
+	private IInternRequestService iInternRequestService;
 
-    @Autowired
-    public InternRequestController(IInternRequestService iInternRequestService) {
-        this.iInternRequestService = iInternRequestService;
-    }
+	@Autowired
+	public InternRequestController(IInternRequestService iInternRequestService) {
+		this.iInternRequestService = iInternRequestService;
+	}
 
 	@Override
 	@PostMapping("/add")
@@ -72,48 +73,58 @@ public class InternRequestController implements IInternRequestController {
 		}
 	}
 
+	@Override
+	@PutMapping("/update/{inteRequeId}")
+	public ResponseEntity<InternRequest> updateInternRequest(@PathVariable("inteRequeId") long inteRequId,
+			@RequestBody InternRequest internRequest) {
+		Optional<InternRequest> inteRequestOptional = Optional
+				.of(iInternRequestService.searchInternRequest(inteRequId));
+		if (inteRequestOptional.isPresent()) {
+			return new ResponseEntity<>(iInternRequestService.updateInternRequest(internRequest), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
 
-    @Override
-    @PutMapping("/update/{inteRequeId}")
-    public ResponseEntity<InternRequest> updateInternRequest(@PathVariable("inteRequeId") long inteRequId, @RequestBody InternRequest internRequest) {
-        Optional<InternRequest> inteRequestOptional = Optional
-                .of(iInternRequestService.searchInternRequest(inteRequId));
-        if (inteRequestOptional.isPresent()) {
-            return new ResponseEntity<>(iInternRequestService.updateInternRequest(internRequest), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
+	@Override
+	@GetMapping("/{inteRequeId}")
+	public ResponseEntity<InternRequest> getInternRequest(@PathVariable("inteRequeId") long inteRequId) {
 
-    @Override
-    @GetMapping("/{inteRequeId}")
-    public ResponseEntity<InternRequest> getInternRequest(@PathVariable("inteRequeId") long inteRequId) {
-
-        Optional<InternRequest> inteRequestOptional = Optional
-                .of(iInternRequestService.searchInternRequest(inteRequId));
-        if (inteRequestOptional.isPresent()) {
-            return new ResponseEntity<>(inteRequestOptional.get(), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-
-    }
-
-    @Override
-    @DeleteMapping("/{inteRequeId}")
-    public ResponseEntity<HttpStatus> deleteInternRequest(@PathVariable("inteRequeId") long inteRequId) {
-        try {
-            iInternRequestService.deleteInternRequest(inteRequId);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+		Optional<InternRequest> inteRequestOptional = Optional
+				.of(iInternRequestService.searchInternRequest(inteRequId));
+		if (inteRequestOptional.isPresent()) {
+			return new ResponseEntity<>(inteRequestOptional.get(), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
 
 	}
 
 	@Override
+	@DeleteMapping("/{inteRequeId}")
+	public ResponseEntity<HttpStatus> deleteInternRequest(@PathVariable("inteRequeId") long inteRequId) {
+		try {
+			iInternRequestService.deleteInternRequest(inteRequId);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+	}
+
+	@Override
+	@GetMapping()
 	public ResponseEntity<List<InternRequest>> getInternRequests() {
-		return null;
+		try {
+			List<InternRequest> internRequests = iInternRequestService.internRequests();
+			if (internRequests.isEmpty()) {
+				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+			}
+			return new ResponseEntity<>(internRequests, HttpStatus.OK);
+
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 
 	@Override
@@ -143,12 +154,10 @@ public class InternRequestController implements IInternRequestController {
 		return null;
 	}
 
-
 	@Override
 	@GetMapping("/internRequestByCareer/{careId}")
 	public ResponseEntity<?> findInternRequestsByCareId(@PathVariable("careId") long careId) {
 		return new ResponseEntity<>(iInternRequestService.findInternRequestsByCareId(careId), HttpStatus.OK);
 	}
-
 
 }
